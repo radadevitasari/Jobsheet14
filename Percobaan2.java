2adc4ac827fccf55a37db6624fc12fc983df5f3f
radadevitasari <radadevitasari87.com>
2025-12-16 22:01:27 +0700

    tambah kode program pertanyaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -4,8 +4,10 @@ public class Percobaan2 {
 
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
+            System.out.print("1");
             return 1; 
         } else {
+            System.out.print(x + "x");
             return x * hitungPangkat(x, y - 1); 
         }
     }
@@ -17,7 +19,8 @@ public class Percobaan2 {
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil);
     }
 }
     
